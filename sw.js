39318bb6ded72793acaacee576caa8cfff0aa349
prhasn <hasn@e.marnd.com>
2023-03-20 01:37:50 +0700

    update

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const cacheName = 'calculator_pwa';
-
-self.addEventListener('install', e => e.waitUntil(
-	caches.open(cacheName).then(cache =>
-		cache.addAll([
-			'/index.html',
-			'/main.css',
-			'/main.js',
-		]).then(() => self.skipWaiting()))
-))
-self.addEventListener('activate', e => e.waitUntil(
+const cacheName = `calculator_pwa`;
+self.addEventListener(`install`, e => e.waitUntil(
+	caches.open(cacheName).then(files =>
+		files.addAll([
+			`./index.html`,
+			`./main.css?23031901`,
+			`./main.js?23031901`,
+			`./manifest.json`,
+			`./sw.js`
+		]).then(() => self.s$kipWaiting()))
+));
+self.addEventListener(`activate`, e => e.waitUntil(
 	caches.keys().then(cacheNames => Promise.all(
-		cacheNames.map(cache =>
-			cache !== cacheName && caches.delete(cache)
+		cacheNames.map(file =>
+			file !== cacheName && caches.delete(file)
 		)
 	))
-))
-self.addEventListener('fetch', e => e.respondWith(
+));
+self.addEventListener(`fetch`, e => e.respondWith(
 	fetch(e.request)
 		.then(res => {
 			const resClone = res.clone();
 			caches.open(cacheName)
-				.then(cache => {
-					cache.put(e.request, resClone);
+				.then(file => {
+					file.put(e.request, resClone);
 				});
 			return res;
 		}).catch(
